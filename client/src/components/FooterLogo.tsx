1ded137907d0ba3a3608fc380e7e8a3b6ee86897
TheCodingBarista <66879810+KeenanFernandes2000@users.noreply.github.com>
2025-05-23 08:45:26 +0000

    fixed logo

--- client/src/components/FooterLogo.tsx
+++ client/src/components/FooterLogo.tsx
@@ -2,11 +2,12 @@ interface FooterLogoProps {
   className?: string;
   height?: number;
 }
+import logoDark from "@assets/Potential.com Logos Main-02.png";
 
 export function FooterLogo({ className = "", height = 40 }: FooterLogoProps) {
   return (
     <img
-      src="/Potential.com Logos Main-02.png"
+      src={logoDark}
       alt="Potential.com Logo"
       height={height}
       className={`h-auto ${className}`}
